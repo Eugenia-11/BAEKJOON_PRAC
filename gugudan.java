e16a63cb2b77cda4820c225b8879aef69162214c
Yulow-J <83537524+Yulow-J@users.noreply.github.com>
2023-01-19 23:14:05 +0900

    Add files via upload

---
+++ gugudan.java
@@ -0,0 +1,17 @@
+package BJ3;
+
+import java.util.Scanner;
+
+public class gugudan {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);	
+		int a = in.nextInt();
+		
+		in.close();		
+		
+		for(int i = 1; i<10;i++) {
+			System.out.println(a+" * "+i+" = "+(a*i));
+		}
+	}
+}
