aee88c52efb259f45f4b3ec3b4fe93bea25b4a6c
Yulow-J <83537524+Yulow-J@users.noreply.github.com>
2023-01-21 19:40:20 +0900

    Add files via upload

---
+++ morex.java
@@ -0,0 +1,25 @@
+package BJ4;
+
+import java.util.Scanner;
+
+public class morex {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		int X = in.nextInt();
+		int arr[] = new int[N];
+        
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+ 
+		in.close();
+        
+		for (int i = 0; i < N; i++) {
+			if (arr[i] < X) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
