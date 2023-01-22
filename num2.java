e3ee2d44986bdc399002db62b8fc2008a1cf9c07
Yulow-J <83537524+Yulow-J@users.noreply.github.com>
2023-01-22 21:08:37 +0900

    Add files via upload

---
+++ num2.java
@@ -0,0 +1,40 @@
+package BJ5;
+
+import java.util.Scanner;
+
+public class num2 {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print(arithmetic_sequence(in.nextInt()));
+		in.close();
+ 
+		
+	}
+ 
+	public static int arithmetic_sequence(int num) {
+		int cnt = 0; 
+ 
+		if (num < 100) {
+			return num;
+		}
+ 
+		else {
+			cnt = 99;
+ 
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100; 
+				int ten = (i / 10) % 10; 
+				int one = i % 10;
+ 
+				if ((hun - ten) == (ten - one)) { 
+					cnt++;
+				}
+			}
+		}
+ 
+		return cnt;
+	}
+ 
+}
