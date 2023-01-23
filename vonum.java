3b48e666b2c0cd85b9900ebe4653b0fe2132a67c
Yulow-J <83537524+Yulow-J@users.noreply.github.com>
2023-01-23 22:52:22 +0900

    Add files via upload

---
+++ vonum.java
@@ -0,0 +1,22 @@
+package BJ6;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+ 
+public class vonum {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		String S = in.nextLine();
+		in.close();
+ 
+		
+		StringTokenizer st = new StringTokenizer(S," ");
+		
+		
+		System.out.println(st.countTokens());	
+		
+	}
+ 
+}
